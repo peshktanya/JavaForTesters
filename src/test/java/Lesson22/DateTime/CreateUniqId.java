13136b82e5dfb68e9566f2b3cd6982b3e3037ba5
Tetiana <peshkovat@ukr.net>
2018-08-12 17:08:48 +0300

    Add new time test

---
+++ src/test/java/Lesson22/DateTime/CreateUniqId.java
@@ -0,0 +1,18 @@
+package Lesson22.DateTime;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CreateUniqId {
+
+    @Test
+    public void createAUniqueUserID(){
+
+        String userID = "user" + System.currentTimeMillis();
+
+        System.out.println(userID);
+        Assert.assertTrue(userID.startsWith("user"));
+        Assert.assertTrue(Long.valueOf(userID.replace("user",""))>1000L);
+    }
+
+}
